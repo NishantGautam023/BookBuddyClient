86a18bd88d6e15a48e47431883bf79a18ac85816
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-04-28 16:51:25 -0400

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="border-t p-5 text-center">
+            <span>© 2024 NisgauCorp&#39;s Book Buddy All rights reserved.</span>
+        </footer>
+    );
+};
+
+export default Footer;
